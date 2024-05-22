f1b0f02fa7d3fa9d7787cdacbe27576f99a118a3
Bryce-VE <brycevanewyk4@gmail.com>
2024-05-22 12:18:06 +1000

    fix errors showing up in debugger

--- godot_game/LoadButton.gd
+++ godot_game/LoadButton.gd
@@ -13,12 +13,10 @@ func load_all_data():
 		return
 	
 	var save_file = FileAccess.open(Globals.SAVE_FILE, FileAccess.READ)
-	var save_nodes = get_tree().get_nodes_in_group(Globals.SAVE_GROUP)
 	
 	while save_file.get_position() < save_file.get_length():
 		var json_string = save_file.get_line()
-		var json = JSON.new()
-		var parsed_data = json.parse_string(json_string)
+		var parsed_data = JSON.parse_string(json_string)
 		
 		# check data has necessary values
 		if Globals.PATH not in parsed_data or Globals.DATA not in parsed_data:
@@ -33,5 +31,5 @@ func load_all_data():
 		if !node.has_method(LOAD):
 			print("ERROR: Node '%s' doesnt have a %s() function" % [node.name, LOAD])
 			continue
-		var node_data = node.call(LOAD, data)
+		node.call(LOAD, data)
 	print("Loaded data!")
