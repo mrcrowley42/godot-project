4f34bbfcd951833f51e36fa40821ecaa22524a93
Bryce-VE <brycevanewyk4@gmail.com>
2024-05-24 13:51:32 +1000

    save in binary & save metadata

--- godot_game/SaveButton.gd
+++ godot_game/SaveButton.gd
@@ -10,9 +10,15 @@ func save_all_data():
 	var save_file = FileAccess.open(Globals.SAVE_FILE, FileAccess.WRITE)
 	var save_nodes = get_tree().get_nodes_in_group(Globals.SAVE_GROUP)
 	
+	var all_data = []
+	
+	# save metadata
+	var metadata = {"time_saved": Time.get_unix_time_from_system()}
+	all_data.append(metadata)
+	
+	# save node data
 	for node in save_nodes:
-		# object doesnt have save() func
-		if !node.has_method(SAVE):
+		if !node.has_method(SAVE):  # object doesnt have save() func
 			print("Node '%s' doesnt have a %s() function" % [node.name, SAVE])
 			continue
 		
@@ -20,7 +26,9 @@ func save_all_data():
 			Globals.PATH: node.get_path(),
 			Globals.DATA: node.call(SAVE)
 		}
-		var serialized_data = JSON.stringify(node_data)
-		
-		save_file.store_line(serialized_data)
+		all_data.append(JSON.stringify(node_data))
+	
+	var binary_data: PackedByteArray = var_to_bytes(all_data)
+	save_file.store_var(binary_data)
+	
 	print("Saved data!")
