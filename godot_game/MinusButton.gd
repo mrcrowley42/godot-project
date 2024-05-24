f6cb7941ec6cf0af46a71b82f0069245648c2080
mrcrowley42 <mattcrowley42@me.com>
2024-05-24 13:53:07 +1000

    added magic pipe

--- godot_game/MinusButton.gd
+++ godot_game/MinusButton.gd
@@ -1,13 +1,13 @@
 extends Button
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-
 	
 func _on_button_down():
-	get_node("../../Player").health -= 10
+	var hp = get_node("../../Player")
+	
+	
+	hp.health -= 10
+	
+	#get_node("../../Player").health = clamp(get_node("../../Player").health,0, 100)
