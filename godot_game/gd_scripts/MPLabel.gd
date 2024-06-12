7779a11064a303ee6d453902018d25156c0416e4
Bryce-VE <brycevanewyk4@gmail.com>
2024-06-12 15:02:42 +1000

    better debug rate

--- godot_game/gd_scripts/MPLabel.gd
+++ godot_game/gd_scripts/MPLabel.gd
@@ -3,7 +3,9 @@ extends Label
 var og_text = self.text
 
 func _process(_delta):
+	var stats = get_node("..").stat_man
+	var rate = -(stats.mp_amount * stats.mp_rate)
 	self.text = og_text % [
 			str(get_node("..").creature.mp), 
-			str(get_node("..").stat_man.mp_rate)
+			str(rate)
 		]
