5268e1adbdb154476c74f4493b6775302b2c5113
mrcrowley42 <mattcrowley42@me.com>
2024-06-16 19:50:13 +1000

    added ability to pause game

--- godot_game/gd_scripts/optionbut.gd
+++ godot_game/gd_scripts/optionbut.gd
@@ -1,11 +1,6 @@
 extends Button
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
+@export var pause_game: bool = false
 func _on_button_down():
+	if pause_game:
+		get_tree().paused = !get_tree().paused
 	%OptionsMenu.visible = !%OptionsMenu.visible
-
