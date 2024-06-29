303d05a82fd6b2a87fa916d2a8aa318f07e7d8ba
mrcrowley42 <mattcrowley42@me.com>
2024-06-29 11:40:22 +1000

    updated names of panels, added resizing, and snapping to slider

--- godot_game/scripts/ActButton.gd
+++ godot_game/scripts/ActButton.gd
@@ -1,6 +1,6 @@
 extends TextureButton
 @export var palettes: Array[Texture2D]
-@onready var act_menu = %KillControls
+@onready var act_menu = %ActivityControls
 @onready var options_menu = %OptionsMenu
 
 func _on_button_down():
--- godot_game/scripts/foodbutton.gd
+++ godot_game/scripts/foodbutton.gd
@@ -1,6 +1,6 @@
 extends TextureButton
 @export var palettes: Array[Texture2D]
-@onready var food_menu = %SaveControls
+@onready var food_menu = %FoodControls
 @onready var options_menu = %OptionsMenu
 
 func _on_button_down():
