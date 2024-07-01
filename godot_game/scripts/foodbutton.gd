769b6b2492319756d313cfc4334a8a534a95b726
mrcrowley42 <mattcrowley42@me.com>
2024-07-01 15:13:59 +1000

    Added pressed state to buttons and click sound

--- godot_game/scripts/foodbutton.gd
+++ godot_game/scripts/foodbutton.gd
@@ -4,5 +4,7 @@ extends TextureButton
 @onready var options_menu = %OptionsMenu
 
 func _on_button_down():
+	%BtnClick.play()
+	texture_pressed = load(texture_normal.resource_path.trim_suffix(".png") + "_pressed.png")
 	if not options_menu.visible:
 		food_menu.visible = !food_menu.visible
--- godot_game/scripts/setting_button.gd
+++ godot_game/scripts/setting_button.gd
@@ -12,4 +12,6 @@ func _process(_delta):
 
 
 func _on_button_down():
+	%BtnClick.play()
+	texture_pressed = load(texture_normal.resource_path.trim_suffix(".png") + "_pressed.png")
 	%OptionsMenu.visible = !%OptionsMenu.visible
