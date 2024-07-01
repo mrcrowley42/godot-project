201e84d21c9a5c8d5a71610ba74cb88b15673470
Bryce-VE <brycevanewyk4@gmail.com>
2024-07-01 11:47:30 +1000

    unload minigame

--- godot_game/scripts/SceneManager.gd
+++ godot_game/scripts/SceneManager.gd
@@ -11,13 +11,16 @@ func loadMinigame(minigame):
 		find_parent("Game").find_child("UI").add_child(game)
 		currentMinigame = minigame.resource_name
 
-# todo: call this somewhere
 func unLoadMinigame():
 	currentMinigame = null;
 
+func _notification(noti):
+	if noti == Globals.NOTIFICATION_MINIGAME_CLOSED:
+		unLoadMinigame()
+
 
 ## Tetris
-func _on_tetris_btn_button_down():
+func _on_tetris_button_down():
 	loadMinigame(tetrisScene)
 
 
@@ -25,6 +28,3 @@ func _on_tetris_btn_button_down():
 func _on_memory_game_button_down():
 	loadMinigame(memoryGameScene)
 
-
-func _on_tetris_button_down():
-	pass # Replace with function body.
