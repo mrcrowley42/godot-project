e80d12ee34e2b24364b66609bd5a903131de8c26
LCameron905 <lcameron2694@gmail.com>
2024-07-02 15:42:27 +1000

    egg stuff

--- godot_game/Creatures/egg.gd
+++ godot_game/Creatures/egg.gd
@@ -3,12 +3,20 @@ extends Node2D
 #var to set wait time until egg hatches 
 @export var wait_time: float 
 
-var Hatching = Timer
 
-
-func _ready():
 	
-	Hatching.start
+func egg_timer():
+	var hatching = Timer.new()
+	hatching.wait_time = wait_time
+	hatching.autostart = true
+	hatching.one_shot = true
+	add_child(%Timer)
+
+	
+func _ready():
+	egg_timer()
 	
 func _process(delta):
 	pass
+
+
