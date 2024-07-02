7f9cfd37359234c61556e19a714765e81d8cdb5f
mrcrowley42 <mattcrowley42@me.com>
2024-07-02 14:28:24 +1000

    added rich text to credits

--- godot_game/scripts/minimax.gd
+++ godot_game/scripts/minimax.gd
@@ -26,9 +26,9 @@ func normalise():
 	window.size.y = start_size.y * 1
 
 func maximise():
-	window.size.x = start_size.x * 1.25
-	window.size.y = start_size.y * 1.25
-	#print(str(OS.get_screen_size()))
+	window.size.x = start_size.x * 1.33
+	window.size.y = start_size.y * 1.33
+	
 
 func _on_normal_button_button_down():
 	window.size = start_size
