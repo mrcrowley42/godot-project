98b267b6d29b8a63182b63344bfbf21377bded07
mrcrowley42 <mattcrowley42@me.com>
2024-07-02 21:58:11 +1000

    scissors paper rock gameplay added

--- godot_game/scenes/scissors_paper_rock.gd
+++ godot_game/scenes/scissors_paper_rock.gd
@@ -1,58 +1,45 @@
+@icon("res://icons/controller-icon.svg")
 extends Node
+@onready var og_text = %ScoreLabel.text
 
+var creature_score: int = 0
+var player_score: int = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	%ScoreLabel.text = og_text % [player_score, creature_score]
+
+func win():
+	player_score += 1
+	%GameStatus.text = 'Victory\nYou win'
+	
+func lose():
+	creature_score += 1
+	%GameStatus.text = 'Defeat\nCreature wins'
+	
+func play(user_choice):
+	var creature_choice = ['rock', 'paper', 'scissors'].pick_random()
+	%CreatureChoice.text = str('Creature chose ' + creature_choice)
+#
+	if creature_choice == user_choice:
+		%GameStatus.text = 'Draw\nNeither wins'
+#
+	elif user_choice == 'rock' and creature_choice == 'scissors':
+		win()
+
+	elif user_choice == 'paper' and creature_choice == 'rock':
+		win()
+
+	elif user_choice == 'scissors' and creature_choice == 'paper':
+		win()
+		
+	else: lose()
 
 
+func _on_scissors_btn_button_down():
+	play("scissors")
 
-#import random
-#
-#creature_score = 0
-#player_score = 0
-#
-#def win():
-	#global player_score
-	#global creature_score
-	#player_score += 1
-	#print('Victory. You win.')
-	#print('Player score:', player_score, 'Creature Score:', creature_score)
-#
-#def lose():
-	#global player_score
-	#global creature_score
-	#computer_score += 1
-	#print('Defeat. Creature wins.')
-	#print('Player score:', player_score, 'Creature Score:', creature_score)
-#
-#def play():
-	#creature_choice = random.choice(['rock', 'paper', 'scissors'])
-	#user_choice = input('Scissors, paper, or rock?\n')
-#
-	#print('Creature chooses', creature_choice)
-#
-	#if creature_choice == user_choice:
-		#print('Draw. Neither wins.')
-		#print('Player score:', player_score, 'Creature Score:', creature_score)
-#
-	#elif user_choice == 'rock' and creature_choice == 'scissors':
-		#win()
-#
-	#elif user_choice == 'paper' and creature_choice == 'rock':
-		#win()
-#
-	#elif user_choice == 'scissors' and creature_choice == 'paper':
-		#win()
-#
-	#else: lose(0
-#
-#game = "y"
-#
-#while game =="y"
-	#play()
+func _on_paper_btn_button_down():
+	play("paper")
+
+func _on_rock_btn_button_down():
+	play("rock")
