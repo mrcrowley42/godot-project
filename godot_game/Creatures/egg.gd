7cd9acd63a71558175713154acf580033b5fcb8d
LCameron905 <lcameron2694@gmail.com>
2024-07-02 23:36:42 +1000

    new egg

--- godot_game/Creatures/egg.gd
+++ godot_game/Creatures/egg.gd
@@ -1,17 +1,24 @@
 extends Node2D
 @onready var cracker = %eggTimer
+@export var set_wait: float
 
-func timerset():
-	%eggTimer.wait_time = 2
-	%eggTimer.one_shot = true
-	%eggTimer.autostart = true
+func party_time():
+	pass
 
-
+func scene_set():
+	%lilGuy.visible = false
+	%eggSprite.visible = true
+	cracker.wait_time = set_wait
 
 func _ready():
-	timerset()
+	scene_set()
+	
 	cracker.timeout.connect(func():
-		print("done"))
+		print("done")
+		%Yip.play()
+		%eggSprite.visible = false
+		%lilGuy.visible = true
+		)
 
 
 
