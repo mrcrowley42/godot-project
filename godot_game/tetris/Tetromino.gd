a7c78ffa5a9507efec700b6cff92f99d753d0853
Bryce-VE <brycevanewyk4@gmail.com>
2024-07-03 13:32:25 +1000

    proper grid snapping

--- godot_game/tetris/Tetromino.gd
+++ godot_game/tetris/Tetromino.gd
@@ -3,21 +3,22 @@ extends Node2D
 @onready var canvas = find_child("CanvasLayer")
 @onready var texture = find_child("Texture")
 var boardSize: Vector2
+var squareSize: Vector2 = Vector2(30, 30)
 
 func snap_to_grid(vec: Vector2, size: Vector2) -> Vector2:
-	vec = floor(vec) - (size / 2)  # get top left of given thing
-	var mod = Vector2(int(vec.x) % 30, int(vec.y) % 30)
-	return vec - (mod * 2)
+	var sMod = Vector2(
+			int(size.x) % int(squareSize.x * 2), 
+			int(size.y) % int(squareSize.y * 2)
+		)
+	return vec - (squareSize / 2) * sMod.normalized()
 
 func init(piece: String, bPos: Vector2, bSize: Vector2):
 	boardSize = bSize
 	canvas.offset = bPos
 	texture.animation = piece
-	
-	place_tet()
 
-func place_tet():
-	texture.position = snap_to_grid(texture.position, texture.get_size())
+func place_tet(pos: Vector2):
+	texture.position = snap_to_grid(pos, texture.get_size())
 
 func gravity_tick():
 	print("fall")
