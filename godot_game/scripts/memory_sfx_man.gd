371e84c7f49acbbe96c2c3286920749e29b99bb3
mrcrowley42 <mattcrowley42@me.com>
2024-07-06 13:21:58 +1000

    added draw sfx, changed various audio streams to sfx bus

--- godot_game/scripts/memory_sfx_man.gd
+++ godot_game/scripts/memory_sfx_man.gd
@@ -2,8 +2,8 @@ extends AudioStreamPlayer
 
 var correct = preload("res://sound_effects/correct.wav")
 var wrong = preload("res://sound_effects/wrong.wav")
-
-var lookup= {"correct": correct, "wrong": wrong}
+var draw = preload("res://sound_effects/confirm.wav")
+var lookup= {"draw": draw, "correct": correct, "wrong": wrong}
 
 func play_sound(sound_name):
 	stream = lookup[sound_name]
