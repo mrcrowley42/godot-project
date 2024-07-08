dd490e96bc48a3c7f3c106346cedd2e51d836210
mrcrowley42 <mattcrowley42@me.com>
2024-07-08 15:07:17 +1000

    arrow scale added back

--- godot_game/scripts/arrowscale.gd
+++ godot_game/scripts/arrowscale.gd
@@ -5,13 +5,13 @@ var toggle = false
 var xoffset = 400
 
 func _ready():
-	#var tween = create_tween().set_loops().set_ease(Tween.EASE_OUT_IN)
+	var tween = create_tween().set_loops().set_ease(Tween.EASE_OUT_IN)
 	var bitmap = BitMap.new()
 	var image = texture_normal.get_image()
 	bitmap.create_from_image_alpha(image)
 	texture_click_mask = bitmap
-	#tween.tween_property($".","scale",Vector2(1.125,1.05), 1)
-	#tween.tween_property($".","scale",Vector2(1,.8), 1)
+	tween.tween_property($".","scale",Vector2(1.125,1.05), 1)
+	tween.tween_property($".","scale",Vector2(1,.8), 1)
 	xoffset = init_pos.x- xoffset
 	
 
