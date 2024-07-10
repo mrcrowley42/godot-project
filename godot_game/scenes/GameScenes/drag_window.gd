6d9ef62f5e63b2d7b34ee5ea797097ae9a3bdeb3
mrcrowley42 <mattcrowley42@me.com>
2024-07-10 13:06:23 +1000

    dragging works!

--- godot_game/scenes/GameScenes/drag_window.gd
+++ godot_game/scenes/GameScenes/drag_window.gd
@@ -3,16 +3,30 @@ extends TextureRect
 #var drag_pos = null
 
 var dragging: bool = false
+var offset = Vector2i(0, 0)
+@onready var window: Window = get_window()
+
+func _process(delta):
+	if dragging:
+		window.position = DisplayServer.mouse_get_position()  - Vector2i(offset)
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		var container_rect = Rect2(self.position, self.size)
 		if container_rect.has_point(event.position):
 			dragging = true
+			offset = position
+			offset = get_global_mouse_position()
 	if event is InputEventMouseButton and not event.pressed:
 		dragging = false
-	if dragging and event is InputEventMouseMotion:
-		get_window().position += Vector2i(event.relative)
+	#if dragging and event is InputEventMouseMotion:
+		#
+		#var rand = RandomNumberGenerator.new()
+#
+		#var width = rand.randi_range(0, 1920- window.size.x)
+		#var height = rand.randi_range(0, 1080 - window.size.y)
+		#window.position = Vector2(width, height)
+		#window.position += Vector2i(event.relative)
 
 #func _on_gui_input(event):
 	#if event is InputEventMouseButton:
