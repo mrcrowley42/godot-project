bb22b6983f17c12305d366847f82a3b59de57327
Bryce-VE <brycevanewyk4@gmail.com>
2024-07-29 09:46:54 +1000

    day percent & minor shader clean

--- godot_game/scripts/background.gd
+++ godot_game/scripts/background.gd
@@ -4,16 +4,20 @@ class_name Background extends ScriptNode
 @onready var bg_light_shader: ColorRect = find_child("LightShader")
 @onready var bg_tint: ColorRect = find_child("ThemeTint")
 
+const SECS_PER_DAY = 86400
 var tint_opacity: float = 30.0
 
 # time values
-var current_time: float = 0.0
+var day_percent: float = 0.0
 
 func _ready():
 	update_bg()
 
 func update_bg():
-	current_time = Time.get_unix_time_from_system()
+	var time = Time.get_time_dict_from_system()
+	var seconds_today = (time.hour * 3600) + (time.minute * 60) + time.second
+	day_percent = float(seconds_today) / float(SECS_PER_DAY)
+	print(day_percent)
 
 func _process(_delta):
 	bg_tint.color = %ScreenColours.color
