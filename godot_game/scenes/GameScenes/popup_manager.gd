3500bae1135b5d1af503fbc38c17b58c09d67cf7
mrcrowley42 <mattcrowley42@me.com>
2024-08-17 19:45:43 +1000

    Update popup_manager.gd

--- godot_game/scenes/GameScenes/popup_manager.gd
+++ godot_game/scenes/GameScenes/popup_manager.gd
@@ -5,12 +5,14 @@ extends Node2D
 # spawn notification in the horizontal centre and just above the top of screen.
 @onready var start_pos = Vector2((540-toast2.size.x)/2,-toast2.size.y+112)
 
-func new_notification(message):
+## Creates a toast notification with the passed string.
+func new_notification(message: String):
+	# Checks every second that the node has no children before executing
+	# Seems to queue consecutive calls ¯\_(ツ)_/¯ 
 	while get_child_count() != 0:
 		await get_tree().create_timer(1).timeout
 		if get_child_count() == 0:
 			continue
-
 	var notif = toast.instantiate()
 	notif.message = message
 	notif.position = start_pos
