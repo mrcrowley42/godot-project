085e41311cd8040b61c0e4fd4cb20ee1af5297d4
LCameron905 <lcameron2694@gmail.com>
2024-08-23 11:05:44 +1000

    added comments

--- godot_game/scripts/MainGameScripts/EggScene/egg.gd
+++ godot_game/scripts/MainGameScripts/EggScene/egg.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+## sets variables when scene is loaded 
 @onready var cracker: Timer = %EggTimer
 @onready var creature: AnimatedSprite2D = creature_selector()
 @export var skip_scene: bool
@@ -15,6 +15,7 @@ func _ready() -> void:
 		await get_tree().process_frame
 		get_tree().change_scene_to_file("res://scenes/GameScenes/prototype.tscn")
 	
+	## process for when the egg timer ends, "pirnts()" are for process checking 
 	cracker.timeout.connect(func() -> void:
 		print(creature)
 		print("done")
@@ -25,11 +26,13 @@ func _ready() -> void:
 		%StartGame.start()
 		)
 	
+	## process for after start game timer ends, timer is for slight delay to give time for users to process 
 	%StartGame.timeout.connect(func() -> void:
 		print("done")
 		get_tree().change_scene_to_file("res://scenes/GameScenes/prototype.tscn")
 		)
 
+## function for selecting which character to use, can be used as condition for creature class construction
 func creature_selector():
 	var array = [
 		%LilGuy,
