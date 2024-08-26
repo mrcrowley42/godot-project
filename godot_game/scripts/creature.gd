31105c38362dddd6320a30388fe0e6b43f5ec245
mrcrowley42 <mattcrowley42@me.com>
2024-08-26 15:08:41 +1000

    Tidy creature script and update food menu for debuging

--- godot_game/scripts/creature.gd
+++ godot_game/scripts/creature.gd
@@ -1,49 +1,54 @@
 @icon("res://icons/class-icons/creature.svg")
-extends Node2D
-
-class_name Creature
 ## Creature base class.
+class_name Creature extends Node2D
+
+# Grab reference to the main sprite so it can be manipulated
+@onready var main_sprite = %Main
 
 ## Colour to tint creature as HP approaches 0.
-@export_color_no_alpha var dying_colour: Color;
+@export var dying_colour: Color;
 @export var max_hp: float = 1000
 @export var max_water: float = 1000
 @export var max_food: float = 1000
 @export var max_fun: float = 1000
+## XP required for the creature to reach the [param ADULT] [param LifeStage] stage
 @export var xp_required: float
-@onready var main_sprite = %Main
 @export var clippy_area: Node
 @export var xp_mulitplier: float = 1.0
 
+# ENUMS
+enum FoodItem {TOAST, CHIPS, FRUIT}
 enum LifeStage {CHILD, ADULT}
 
+# CREATURE STATS VARIABLES
 var hp: float
 var water: float
 var food: float
 var fun: float
 var xp: float
 var life_stage: LifeStage
+var likes: Array = [FoodItem.CHIPS]
+var dislikes: Array = [FoodItem.FRUIT]
 
+# SIGNALS
 signal hp_changed()
 signal food_changed()
 signal water_changed()
 signal fun_changed()
 signal xp_changed()
 
+# Map shorthand strings to corresponding damage function
 var stats: Dictionary = {
-	'hp': damage_hp,
-	'fun': damage_fun,
-	"water": damage_water,
-	'food': damage_food
-}
-
-func add_xp(amount):
+	'hp': damage_hp, 'fun': damage_fun, "water": damage_water, 'food': damage_food}
+	
+## Add the specified [param amount] to the creature's existing xp.
+func add_xp(amount) -> void:
 	xp += amount
 	xp_changed.emit()
 	if xp >= xp_required:
 		level_up()
 
-func level_up():
+func level_up() -> void:
 	pass
 	#life_stage = LifeStage.Adult
 
@@ -60,7 +65,7 @@ func reset_stats() -> void:
 ## Generialised function to damage/heal the Creature (use a negative value to heal)
 func dmg(amount: float, stat: String) -> void:
 	stats[stat].call(amount)
-	
+
 ## Change to game over scene.
 func game_over():
 	get_tree().change_scene_to_file("res://scenes/GameScenes/game_over.tscn")
@@ -81,7 +86,7 @@ func damage_hp(amount: float) -> void:
 		add_xp(hp - temp_hp * xp_mulitplier)
 	apply_dmg_tint()
 	hp_changed.emit()
-	
+
 func damage_food(amount) -> void:
 	var temp_food = food
 	self.food -= amount
@@ -97,7 +102,7 @@ func damage_fun(amount) -> void:
 	if fun - temp_fun > 0:
 		add_xp(fun - temp_fun * xp_mulitplier)
 	fun_changed.emit()
-	
+
 func damage_water(amount) -> void:
 	var temp_water = water
 	self.water -= amount
@@ -106,10 +111,9 @@ func damage_water(amount) -> void:
 		add_xp(water - temp_water * xp_mulitplier)
 	water_changed.emit()
 
-
 func save() -> Dictionary:
 	return {"water": water, "food": food, "fun": fun, "hp": hp, "xp": xp}
-	
+
 func load(data) -> void:
 	var stat_list = ["water", "fun", "food", "xp"]
 	if not find_parent("Game").debug_mode:
