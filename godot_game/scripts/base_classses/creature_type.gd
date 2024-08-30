5b2837b297cf250e0231cb39fcc376602f0dc618
mrcrowley42 <mattcrowley42@me.com>
2024-08-30 12:24:22 +1000

    move prefs to the bottom of list

--- godot_game/scripts/base_classses/creature_type.gd
+++ godot_game/scripts/base_classses/creature_type.gd
@@ -6,6 +6,6 @@ class_name CreatureType extends Resource
 @export var max_water: int
 @export var max_food: int
 @export var max_fun: int
+@export var sprite_frames: SpriteFrames
 @export var likes: Array[Creature.FoodItem]
 @export var dislikes: Array[Creature.FoodItem]
-@export var sprite_frames: SpriteFrames
