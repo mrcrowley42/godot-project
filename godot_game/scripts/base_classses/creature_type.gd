8182d6041384c63b0eebc5745ea6f182f6064b8a
mrcrowley42 <mattcrowley42@me.com>
2024-08-30 12:28:07 +1000

    Update creature_type.gd

--- godot_game/scripts/base_classses/creature_type.gd
+++ godot_game/scripts/base_classses/creature_type.gd
@@ -7,5 +7,6 @@ class_name CreatureType extends Resource
 @export var max_food: int
 @export var max_fun: int
 @export var sprite_frames: SpriteFrames
+@export_group("Preferences")
 @export var likes: Array[Creature.FoodItem]
 @export var dislikes: Array[Creature.FoodItem]
