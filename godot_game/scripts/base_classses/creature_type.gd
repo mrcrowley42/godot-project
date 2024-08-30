392fc821516950c126b56848b0d3da2c81b8f10e
mrcrowley42 <mattcrowley42@me.com>
2024-08-30 12:04:15 +1000

    added creature type resource

---
+++ godot_game/scripts/base_classses/creature_type.gd
@@ -0,0 +1,10 @@
+class_name CreatureType extends Resource
+## A custom resource to store the information about an individual creature
+
+@export var creature_name: String
+@export var max_hp: int
+@export var max_water: int
+@export var max_food: int
+@export var max_fun: int
+@export var likes: Array[Creature.FoodItem]
+@export var dislikes: Array[Creature.FoodItem]
