5b8ae380e9c2a64857062fcdb0201d218efc591c
Bryce-VE <brycevanewyk4@gmail.com>
2024-08-30 15:17:47 +1000

    shuffle stuff around

---
+++ godot_game/scripts/base_classses/egg_creature_entry.gd
@@ -0,0 +1,4 @@
+class_name EggCreatureEntry extends Resource
+
+@export var creature_type: CreatureType
+@export var weight: int = 1
--- godot_game/scripts/base_classses/egg_entry_class.gd
+++ godot_game/scripts/base_classses/egg_entry_class.gd
@@ -2,4 +2,4 @@ class_name EggEntry extends Resource
 
 @export var name: String
 @export var image: Texture2D
-@export var hatches: Array[String]
+@export var hatches: Array[EggCreatureEntry]
