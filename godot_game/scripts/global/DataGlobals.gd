20cc4d9ca980ba88be3e6ab8b8a7a05f563501bc
Bryce-VE <brycevanewyk4@gmail.com>
2024-08-31 15:13:19 +1000

    clean up DataGlobals a little

--- godot_game/scripts/global/DataGlobals.gd
+++ godot_game/scripts/global/DataGlobals.gd
@@ -6,19 +6,28 @@ const DATA = "data"
 const SAVE = "save"
 const LOAD = "load"
 
+## store the metatada that was last loaded from the save file
+var metadata_last_loaded: Dictionary = {}
+
 func has_save_data() -> bool:
 	return FileAccess.file_exists(Globals.SAVE_DATA_FILE)
+
+func has_settings_data() -> bool:
+	return FileAccess.file_exists(Globals.SAVE_SETTINGS_FILE)
 
 ## --------------
 ##    SAVING
 ## --------------
 
+# save file structure:
+# line 1  = metadata
+# line 2+ = node data  (each node is assigned its own line)
 func save_data():
 	var save_file = FileAccess.open(Globals.SAVE_DATA_FILE, FileAccess.WRITE)
 	var save_nodes = get_tree().get_nodes_in_group(Globals.SAVE_DATA_GROUP)
 	var all_data = []
 
-	var metadata = {"time_saved": Time.get_unix_time_from_system()}
+	var metadata = {"last_opened": Time.get_unix_time_from_system()}
 	all_data.append(metadata)
 
 	# save node data
@@ -60,50 +69,45 @@ func save_settings_data():
 
 ## returns metadata
 func load_data() -> Dictionary:
-	var metadata = {}
-	
-	if not FileAccess.file_exists(Globals.SAVE_DATA_FILE):
-		return {}
-	
-	var save_file = FileAccess.open(Globals.SAVE_DATA_FILE, FileAccess.READ)
-	var file_lines: Array = bytes_to_var(save_file.get_var())
-	metadata["last_opened"] = file_lines.pop_at(0)['time_saved']
-	
-	for line in file_lines:
-		var parsed_line = JSON.parse_string(line)
+	if has_save_data():
+		var save_file = FileAccess.open(Globals.SAVE_DATA_FILE, FileAccess.READ)
+		var file_lines: Array = bytes_to_var(save_file.get_var())
 		
-		# check data has necessary values
-		if PATH not in parsed_line or DATA not in parsed_line:
-			print("ERROR: Missing '%s' or '%s' value for data, skipping" % [PATH, DATA])
-			continue
+		## retrieve metadata
+		metadata_last_loaded = file_lines.pop_at(0)
 		
-		var node = get_node(parsed_line[PATH])
-		if node and node.has_method(LOAD):
-			var data = parsed_line[DATA]
-			node.call(LOAD, data)
-		else:
-			print("ERROR: Node '%s' is null or doesnt have a %s() function" % [parsed_line[PATH], LOAD])
-	return metadata
+		for line in file_lines:
+			var parsed_line = JSON.parse_string(line)
+			
+			# check data has necessary values
+			if PATH not in parsed_line or DATA not in parsed_line:
+				print("ERROR: Missing '%s' or '%s' value for data, skipping" % [PATH, DATA])
+				continue
+			
+			var node = get_node(parsed_line[PATH])
+			if node and node.has_method(LOAD):
+				var data = parsed_line[DATA]
+				node.call(LOAD, data)
+			else:
+				print("ERROR: Node '%s' is null or doesnt have a %s() function" % [parsed_line[PATH], LOAD])
+	return metadata_last_loaded
 
 func load_settings_data():
-	if not FileAccess.file_exists(Globals.SAVE_SETTINGS_FILE):
-		return
-	
-	var config = ConfigFile.new()
-	config.load(Globals.SAVE_SETTINGS_FILE)
-	
-	var settings_nodes = get_tree().get_nodes_in_group(Globals.SAVE_SETTINGS_GROUP)
-	
-	for node in settings_nodes:
-		if !node.has_method(SAVE) or !node.has_method(LOAD): # object doesnt have save() func
-			print("Node '%s' doesnt have a %s() or a %s() function/s" % [node.name, SAVE, LOAD])
-			continue
+	if has_settings_data():
+		var config = ConfigFile.new()
+		config.load(Globals.SAVE_SETTINGS_FILE)
 		
-		var data_to_send = {}
-		var data = node.call(SAVE)
-		var section = data[SECTION] if data.has(SECTION) else Globals.DEFAULT_SECTION
-		
-		for key in data.keys():
-			if key != SECTION and config.has_section(section) and config.has_section_key(section, key):
-				data_to_send[key] = config.get_value(section, key)
-		node.call(LOAD, data_to_send)
+		var settings_nodes = get_tree().get_nodes_in_group(Globals.SAVE_SETTINGS_GROUP)
+		for node in settings_nodes:
+			if !node.has_method(SAVE) or !node.has_method(LOAD): # object doesnt have save() func
+				print("Node '%s' doesnt have a %s() or a %s() function/s" % [node.name, SAVE, LOAD])
+				continue
+			
+			var data_to_send = {}
+			var data = node.call(SAVE)
+			var section = data[SECTION] if data.has(SECTION) else Globals.DEFAULT_SECTION
+			
+			for key in data.keys():
+				if key != SECTION and config.has_section(section) and config.has_section_key(section, key):
+					data_to_send[key] = config.get_value(section, key)
+			node.call(LOAD, data_to_send)
