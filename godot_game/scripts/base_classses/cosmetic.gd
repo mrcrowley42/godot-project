3ea9fcd65aaf7fdad30f1669f4ba49cc2a3de23d
mrcrowley42 <mattcrowley42@me.com>
2024-09-02 12:03:56 +1000

    cosmetic category

--- godot_game/scripts/base_classses/cosmetic.gd
+++ godot_game/scripts/base_classses/cosmetic.gd
@@ -1,7 +1,11 @@
 extends Resource
 class_name CosmeticItem
+
+enum Cosmetic_Category {HAT, GLASSES}
+
 ## A custom resource to store the information about an unlockable cosmetic.
 @export var name: String
 @export_multiline var desc: String
-@export var unlocked: bool = false
+@export var unlocked_by_default: bool = false
 @export var thumbnail: Texture2D
+@export var category: Cosmetic_Category
