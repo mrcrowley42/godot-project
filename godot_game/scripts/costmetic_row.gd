bdbd05da8a1fc78123665b6b5b9b4f6d6f0fbf7d
Bryce-VE <brycevanewyk4@gmail.com>
2024-09-03 08:59:21 +1000

    merg

--- godot_game/scripts/costmetic_row.gd
+++ godot_game/scripts/costmetic_row.gd
@@ -4,6 +4,7 @@ const BTN_SIZE: Vector2 = Vector2(64, 64)
 
 var unlockables = load("res://resources/unlockables.tres")
 @export var category: CosmeticItem.Cosmetic_Category
+
 ## Class that describes the button object for each cosmetic item.
 class UnlockableIcon extends Button:
 	var cosmetic_name
@@ -12,9 +13,10 @@ class UnlockableIcon extends Button:
 		# Universal Styling and sizeing
 		custom_minimum_size = BTN_SIZE
 		size = BTN_SIZE
-		add_theme_constant_override("icon_max_width", 50)
 		theme = load("res://themes/menu_btn.tres")
 		expand_icon = false
+		add_theme_constant_override("icon_max_width", 50)
+		
 		# Individual properites
 		tooltip_text = unlockable.desc
 		icon = unlockable.thumbnail
@@ -29,9 +31,11 @@ class UnlockableIcon extends Button:
 
 func _ready():
 	var unlocked_items = DataGlobals.metadata_last_loaded[DataGlobals.UNLOCKED_ITEMS]
+	
 	for item: CosmeticItem in unlockables.unlockables:
 		var uid = ResourceLoader.get_resource_uid(item.resource_path)
 		var is_unlocked = item.unlocked or uid in unlocked_items
+		
 		if item.category == category:
 			var item_btn = UnlockableIcon.new(item, is_unlocked)
 			add_child(item_btn)
