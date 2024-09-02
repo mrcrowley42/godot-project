dbb070d7c675c8612fcfadc6da7dbe329e08cae2
mrcrowley42 <mattcrowley42@me.com>
2024-09-02 17:14:48 +1000

    fix

--- godot_game/scripts/costmetic_row.gd
+++ godot_game/scripts/costmetic_row.gd
@@ -1,5 +1,7 @@
 extends GridContainer
 
+const BTN_SIZE: Vector2 = Vector2(64, 64)
+
 var unlockables = load("res://resources/unlockables.tres")
 @export var category: CosmeticItem.Cosmetic_Category
 ## Class that describes the button object for each cosmetic item.
@@ -8,9 +10,8 @@ class UnlockableIcon extends Button:
 	
 	func _init(unlockable: CosmeticItem, is_unlocked: bool):
 		# Universal Styling and sizeing
-		custom_minimum_size = Vector2(64,64)
-		size.x = 64
-		size.y = 64
+		custom_minimum_size = BTN_SIZE
+		size = BTN_SIZE
 		add_theme_constant_override("icon_max_width", 50)
 		theme = load("res://themes/menu_btn.tres")
 		expand_icon = false
@@ -18,7 +19,8 @@ class UnlockableIcon extends Button:
 		tooltip_text = unlockable.desc
 		icon = unlockable.thumbnail
 		cosmetic_name = unlockable.name
-		disabled = !unlockable.unlocked 
+		if not is_unlocked or !unlockable.unlocked:
+			disabled = true 
 		
 	## Action when button is pressed.
 	func _pressed():
@@ -26,21 +28,10 @@ class UnlockableIcon extends Button:
 
 
 func _ready():
-	# Dynamically build menu
-	for i in range(5):
-		for item in unlockables.unlockables:
-			if item.category == category:
-				var unlock_item = UnlockableIcon.new(item, false)
-				add_child(unlock_item)
-
-
-## Update menu when an item is unlocked.
-func update_menu():
 	var unlocked_items = DataGlobals.metadata_last_loaded[DataGlobals.UNLOCKED_ITEMS]
-	
 	for item: CosmeticItem in unlockables.unlockables:
 		var uid = ResourceLoader.get_resource_uid(item.resource_path)
 		var is_unlocked = item.unlocked or uid in unlocked_items
-		
-		var item_btn = UnlockableIcon.new(item, is_unlocked)
-		add_child(item_btn)
+		if item.category == category:
+			var item_btn = UnlockableIcon.new(item, is_unlocked)
+			add_child(item_btn)
