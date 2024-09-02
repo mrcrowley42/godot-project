9451c4895a92d8253f8c9b590fbd08e3e4c8c35b
mrcrowley42 <mattcrowley42@me.com>
2024-09-02 10:05:10 +1000

    HAT

--- godot_game/scripts/base_classses/cosmetic.gd
+++ godot_game/scripts/base_classses/cosmetic.gd
@@ -2,5 +2,5 @@ extends Resource
 class_name CosmeticItem
 ## A custom resource to store the information about an unlockable cosmetic.
 @export var name: String
-@export var desc: String
+@export_multiline var desc: String
 @export var unlocked: bool = false
