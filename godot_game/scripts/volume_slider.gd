46ab8f85ef476179e5cd2b4e64a3977345e78231
mrcrowley42 <mattcrowley42@me.com>
2024-09-03 12:29:06 +1000

    hovering over volume gives decimal value

--- godot_game/scripts/volume_slider.gd
+++ godot_game/scripts/volume_slider.gd
@@ -6,11 +6,13 @@ class_name VolumeSlider extends HSlider
 var bus_index: int
 
 func _ready() -> void:
+	tooltip_text = str(value)
 	bus_index = AudioServer.get_bus_index(bus_name)
 	value_changed.connect(_on_value_changed)
 	self.value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
 
 func _on_value_changed(vol_value: float) -> void:
+	tooltip_text = str(value)
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(vol_value))
 
 func save() -> Dictionary:
