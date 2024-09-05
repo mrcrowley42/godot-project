16fbfe635d2a099e91f65c26c3aa6f443e225a30
mrcrowley42 <mattcrowley42@me.com>
2024-09-05 13:20:18 +1000

    WONT LET ME DELETE

--- godot_game/scripts/accessory_manager.gd
+++ godot_game/scripts/accessory_manager.gd
@@ -1,38 +1,44 @@
 class_name AccessoryManager extends ScriptNode
 
 @onready var creature = find_parent("Creature")
-var current_cosmetics
+var current_cosmetics: Array[String]
 var position_dict: Dictionary
+var unlockables = load("res://resources/unlockables.tres").unlockables
+var unlockables_dict: Dictionary
 
 ## Class to define a cosmetic as it appears in game.
 class CosmeticSprite extends AnimatedSprite2D:
 	func _init(cosmetic: CosmeticItem):
-		# TODO add to cosmetic resource
 		self.sprite_frames = cosmetic.sprite
 		# TODO will change depending on whether the correctly scaled versions of sprites are used.
 		self.scale = Vector2(0.225, 0.225)
-		# TODO add to creature resource
 
 
-func _ready():
+func _ready() -> void:
+	for item in unlockables:
+		unlockables_dict[item.name] = item
+	
 	for item in find_parent("Creature").creature_type.cosmetic_positions:
 		position_dict[item.item] = item.position
 	
-	print(position_dict)
 
 ## If the passed cosmetic item isn't already in the scene, and add it, at the location set
 ## for the current creature. If it does exist, remove that instance.
 func toggle_cosmetic(cosmetic: CosmeticItem) -> void:
-	#if not is_inside_tree():
+	if cosmetic.name not in current_cosmetics:
 		var new_sprite = CosmeticSprite.new(cosmetic)
 		new_sprite.name = cosmetic.name
 		new_sprite.position = position_dict[cosmetic]  # lol
 		# possibly link animation up with main sprite so frame rate meets up
 		new_sprite.play()
-		add_child(new_sprite)
-		
-	#else:
+		add_child(new_sprite, true)
+		current_cosmetics.append(cosmetic.name)
+	else:
 		#remove_child(find_child(cosmetic.name))
+		# Painful to do this with an array but whatever
+		for i in range(len(current_cosmetics)):
+			if current_cosmetics[i] == cosmetic.name:
+				current_cosmetics.remove_at(i)
 
 
 func save() -> Dictionary:
@@ -42,4 +48,4 @@ func save() -> Dictionary:
 func load(data) -> void:
 	if data.has("current_cosmetics"):
 		for item in data["current_cosmetics"]:
-			toggle_cosmetic(item)
+			toggle_cosmetic(unlockables_dict[item])
