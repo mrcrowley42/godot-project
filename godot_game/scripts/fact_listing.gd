f102847bdae94eed8d7dd417ea9176e349b5d6e9
mrcrowley42 <mattcrowley42@me.com>
2024-09-17 19:39:30 +1000

    list

---
+++ godot_game/scripts/fact_listing.gd
@@ -0,0 +1,21 @@
+extends MarginContainer
+
+@onready var btn_sfx = find_parent("Game").find_child("BtnClick")
+@onready var heading = find_child("Category")
+var category: Fact.FactCategory
+
+func _ready() -> void:
+	heading.text = Fact.FactCategory.keys()[category] 
+	#content.text = fact.fact
+	#source.text = source.text % [fact.source]
+
+func _on_cancel_button_down() -> void:
+	btn_sfx.play()
+	queue_free()
+
+func _on_hidden() -> void:
+	queue_free()
+
+func add_facts():
+	
+	add_child(fact_btn)
