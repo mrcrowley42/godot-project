125cfabbc20720cfe68ef18679696640518dd3f1
mrcrowley42 <mattcrowley42@me.com>
2024-09-17 14:53:03 +1000

    add fact category buttons

---
+++ godot_game/scripts/fact_topic_btn.gd
@@ -0,0 +1,24 @@
+extends Button
+@export var category: Fact.FactCategory
+
+@onready var fact_window_scene = preload("res://scenes/UiScenes/fact_popup.tscn")
+#@onready var library = find_parent("Library")
+@onready var fact_menu = %FactsMenu
+
+#@onready var fact_window_scene = preload("res://scenes/UiScenes/fact_popup.tscn")
+#@onready var library = find_parent("Library")
+#@onready var fact_menu = %FactsMenu
+
+func _ready() -> void:
+	text = Fact.FactCategory.keys()[category]
+	
+
+func _on_button_down() -> void:
+	# temp stuff
+	var fact_scene = fact_window_scene.instantiate()
+	fact_menu.add_child(fact_scene)
+	
+	print(category)
+
+	#var fact_scene = fact_window_scene.instantiate()
+	#fact_menu.add_child(fact_scene)
