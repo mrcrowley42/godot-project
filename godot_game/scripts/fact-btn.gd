202fb42b4b7403d7fc9dc3354998f741aaa6b235
mrcrowley42 <mattcrowley42@me.com>
2024-09-26 13:33:03 +1000

    refactor fact code

--- godot_game/scripts/fact-btn.gd
+++ godot_game/scripts/fact-btn.gd
@@ -1,12 +1,15 @@
 extends Button
 
 @onready var fact_window_scene = preload("res://scenes/UiScenes/fact_popup.tscn")
-
 var fact_to_display: Fact = load("res://resources/facts/example_fact.tres")
 
-func _ready() -> void:
-	text = fact_to_display.title
-
+func update_locked():
+	var unlocked_items = DataGlobals.load_metadata()['unlocked_facts']
+	var uid = str(ResourceLoader.get_resource_uid(self.fact_to_display.resource_path))
+	var is_unlocked = true if self.fact_to_display.unlocked else uid in unlocked_items
+	self.disabled = !is_unlocked
+	self.text = "?" if self.disabled else fact_to_display.title
+	self.tooltip_text = fact_to_display.hint if disabled else ""
 
 func _on_button_down() -> void:
 	var fact_scene = fact_window_scene.instantiate()
