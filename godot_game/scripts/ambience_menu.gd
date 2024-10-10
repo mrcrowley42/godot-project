308b6d33a207f17ab792f4e24079ff90cc7a499c
mrcrowley42 <mattcrowley42@me.com>
2024-10-10 15:38:54 +1100

    dynamic update ambience dropdowns

--- godot_game/scripts/ambience_menu.gd
+++ godot_game/scripts/ambience_menu.gd
@@ -2,14 +2,20 @@ extends VBoxContainer
 
 @onready var category_btn: OptionButton = find_child("CategoryBtn")
 @onready var sound_btn = find_child("SoundBtn")
+var categories = load("res://resources/ambience_categories.tres").items
 
 func _ready() -> void:
-	# remove placeholder text
+	# remove placeholder text	
 	category_btn.remove_item(0)
-	sound_btn.remove_item(0)
-	
-	# just loading fire here for testing
-	var fire = load("res://resources/ambient_sounds/categories/category_fire.tres")
-	category_btn.add_icon_item(fire.image, fire.category_name)
-	for sound in fire.sound_resources:
+	for category in categories:
+		category_btn.add_icon_item(category.image, category.category_name)
+	category_btn.item_selected.emit(0)
+
+func update_sound_list(category):
+	sound_btn.clear()
+	for sound in category.sound_resources:
 		sound_btn.add_item(sound.sound_name)
+	sound_btn.select(0)
+
+func _on_category_btn_item_selected(index: int) -> void:
+	update_sound_list(categories[index])
---
+++ godot_game/scripts/base_classses/resource_list.gd
@@ -0,0 +1,3 @@
+class_name ResourceList extends Resource
+## Generic resource class to hold a listing of all resources of a certain type.
+@export var items: Array[Resource]
