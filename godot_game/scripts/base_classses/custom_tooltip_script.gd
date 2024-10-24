73417bd3e5f7de774c66ba7fce37e86bd5bba6e4
Bryce-VE <brycevanewyk4@gmail.com>
2024-10-24 15:28:09 +1100

    tooltip style & static positions

--- godot_game/scripts/base_classses/custom_tooltip_script.gd
+++ godot_game/scripts/base_classses/custom_tooltip_script.gd
@@ -1,49 +1,84 @@
 @icon("res://icons/class-icons/chat-square-heart-fill.svg")
 class_name CustomTooltipButton extends Button
 
-var LABEL_THEME = load("res://themes/monospace_font.tres")
-const TOOLTIP_MARGIN: int = 10;
-
-## todo: change this based on current selected theme colour
-var THEME_COLOUR_BEVEL_IMG = load("res://UI stuff/Green Palette/ui_green_box.png")
 var THEME_DICT = {
 	THEME.GREY: load("res://themes/menu_btn.tres"),
 	THEME.DARK_GREY: load("res://themes/menu_btn_dark.tres")
 }
 
-enum THEME {GREY, DARK_GREY, COLOUR_BEVEL}
-enum DIRECTION {AUTO_HORIZONTAL, AUTO_VERTICAL, UP, DOWN, LEFT, RIGHT}
+enum THEME {GREY, DARK_GREY}
+enum DIRECTION {SMART_HORIZONTAL, SMART_VERTICAL, UP, DOWN, LEFT, RIGHT}
 
 @export var tooltip_string: String
 @export var style: THEME
 @export var direction: DIRECTION
 @export var tooltip_scale: float = 1
+@export var margin: int = 8
 
-var tooltip_object: Control;
+var tooltip_object: Label
 
 
 func _ready() -> void:
 	mouse_entered.connect(on_mouse_entered)
 	mouse_exited.connect(on_mouse_exited)
 	
-	tooltip_object = Control.new()
-	var label: Label = Label.new()
-	label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
-	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	label.theme = LABEL_THEME
-	label.text = tooltip_string
-	label.scale = Vector2(tooltip_scale, tooltip_scale)
+	add_child(generate_tooltip_object())
 	
-	tooltip_object.add_child(label)
-	add_child(tooltip_object)
 	tooltip_object.modulate = Color(1, 1, 1, 0)
-	
 	tooltip_text = ""  # override lol
+
+
+func generate_tooltip_object() -> CanvasLayer:
+	tooltip_object = Label.new()
+	tooltip_object.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+	tooltip_object.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	tooltip_object.scale = Vector2(tooltip_scale, tooltip_scale)
+	
+	tooltip_object.theme = THEME_DICT[style]
+	var cl = CanvasLayer.new()
+	cl.layer = 10;
+	cl.add_child(tooltip_object)
+	return cl
+
+
+## find pos of tooltip
+func place_tooltip():
+	tooltip_object.position = global_position
+	
+	var tt_size = tooltip_object.size * tooltip_object.scale
+	var this_size = size * scale
+	
+	# smart positions
+	if direction == DIRECTION.SMART_HORIZONTAL:
+		pass
+		var true_x = global_position.x + (-(tt_size.x - this_size.x) * .5)
+	if direction == DIRECTION.SMART_VERTICAL:
+		pass
+	
+	# static positions
+	if direction == DIRECTION.UP:
+		tooltip_object.position.x += -(tt_size.x - this_size.x) * .5
+		tooltip_object.position.y += -tt_size.y - margin
+	if direction == DIRECTION.DOWN:
+		tooltip_object.position.x += -(tt_size.x - this_size.x) * .5
+		tooltip_object.position.y += this_size.y + margin
+	if direction == DIRECTION.LEFT:
+		tooltip_object.position.y += -(tt_size.y - this_size.y) * .5
+		tooltip_object.position.x += -(tt_size.x + margin)
+	if direction == DIRECTION.RIGHT:
+		tooltip_object.position.y += -(tt_size.y - this_size.y) * .5
+		tooltip_object.position.x += this_size.x + margin
+
+
+## update string & tooltip position
+func update_tooltip():
+	tooltip_object.text = tooltip_string  # refresh text
+	place_tooltip()  # string is different size, re-place
 
 
 func on_mouse_entered():
 	if visible:
-		tooltip_object.get_child(0).text = tooltip_string  # refresh text
+		update_tooltip()
 		Globals.tween(tooltip_object, "modulate", Color(1, 1, 1, 1), 0, .3)
 		#Globals.tween(tooltip_object, "position", og_pos, 0, .3)
 
