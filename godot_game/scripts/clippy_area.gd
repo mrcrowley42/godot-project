05698932df87fd08f815b89ca419e082bdaae434
mrcrowley42 <mattcrowley42@me.com>
2024-11-13 12:38:27 +1100

    failsafe

--- godot_game/scripts/clippy_area.gd
+++ godot_game/scripts/clippy_area.gd
@@ -16,6 +16,7 @@ const MUTE_SETTING = "Mute SFX in Clippy-Mode"
 @onready var ui = %UI
 @onready var bg = %BGCanvasLayer
 @onready var notif_layer = find_parent("Game").find_child("NotificationLayer")
+@onready var sfx_bus = AudioServer.get_bus_index("SFX")
 
 var dragging: bool = false
 var offset = Vector2(0, 0)
@@ -68,6 +69,8 @@ func toggle_clippy_mode() -> void:
 	window.always_on_top = clippy
 	ui.visible = !clippy
 	bg.visible = !clippy
+	
+	AudioServer.set_bus_mute(sfx_bus, clippy)
 
 	# Hide notifications already on screen when going into clippy mode
 	for child in notif_layer.get_child(0).get_children():
