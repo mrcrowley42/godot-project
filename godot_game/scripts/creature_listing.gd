8ead464a55c46f2cc400c611dcb5a7763c3edaef
mrcrowley42 <mattcrowley42@me.com>
2024-11-29 14:32:44 +1100

    update creature listing in codex

---
+++ godot_game/scripts/creature_listing.gd
@@ -0,0 +1,24 @@
+extends Button
+
+@export var name_label: Label
+var creature_data: CreatureType
+
+func _ready() -> void:
+	update_locked()
+
+func has_encountered(creature_type) -> bool:
+	var uid = Helpers.uid_str(creature_type)
+	return uid in DataGlobals.get_metadata_value(DataGlobals.CREATURES_DISCOVERED)
+
+
+func update_locked():
+	self.disabled = !has_encountered(self.creature_data)
+	name_label.text = self.creature_data.name if not self.disabled else "???"
+	#icon = null if self.disabled else cosmetic.thumbnail
+
+func _on_button_down() -> void:
+	var parent = find_parent("CreaturesMenu")
+	var scene = parent.find_child("Creatures").creature_info_scene.instantiate()
+	scene.creature = self.creature_data
+	scene.setup()
+	parent.add_child(scene)
