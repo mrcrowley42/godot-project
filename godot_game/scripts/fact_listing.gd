8138fc7f212f6a97cff6a7ed01fbc967d429619d
Bryce-VE <brycevanewyk4@gmail.com>
2025-01-28 15:12:36 +1100

    fact menu done

--- godot_game/scripts/fact_listing.gd
+++ godot_game/scripts/fact_listing.gd
@@ -4,6 +4,7 @@ extends MarginContainer
 @export var fact_container: Node
 @export var fact_title_label: Label
 @export var fact_body_label: Label
+@export var source_label: Label
 
 @onready var fact_btn_scene = preload("res://scenes/UiScenes/fact_btn.tscn")
 @onready var btn_sfx = find_parent("Game").find_child("BtnClick")
@@ -11,8 +12,11 @@ extends MarginContainer
 var category: Fact.FactCategory
 var facts = load("res://resources/fact_list.tres")
 
+var all_buttons: Array[Button]
+
 func _ready() -> void:
 	heading.text = "Facts: %s" % Fact.FactCategory.keys()[category]
+	show_fact()  # empty
 
 
 func _on_back_button_down() -> void:
@@ -24,8 +28,37 @@ func _on_hidden() -> void:
 	queue_free()
 
 
-func fact_btn_pressed(btn, fact, unlocked):
-	print(btn, fact, unlocked)
+func show_fact(fact: Fact=null, unlocked: bool=true):
+	fact_body_label.modulate.a = 1
+	source_label.text = ""
+	
+	if fact == null:
+		fact_title_label.text = "Select a fact!"
+		fact_body_label.text = ""
+		return
+	
+	if not unlocked:
+		fact_title_label.text = "Unknown"
+		fact_body_label.text = "hint: " + fact.hint
+		fact_body_label.modulate.a = .3
+		return
+	
+	fact_title_label.text = fact.title
+	fact_body_label.text = fact.fact
+	source_label.text = "Source: " + fact.source
+
+func fact_btn_pressed(btn: Button, fact: Fact, unlocked: bool):
+	# no fact
+	if btn.button_pressed:
+		show_fact()
+		return
+	
+	# display fact
+	show_fact(fact, unlocked)
+	for button in all_buttons:
+		if button != btn:
+			button.button_pressed = false
+
 
 func is_unlocked(fact: Fact) -> bool:
 	var unlocked_items = DataGlobals.get_global_metadata_value(DataGlobals.UNLOCKED_FACTS)
@@ -44,6 +77,9 @@ func add_facts() -> void:
 			var btn: Button = fact_btn_scene.instantiate()
 			var unlocked = is_unlocked(fact)
 			btn.text = str(i) if unlocked else "???"
+			btn.toggle_mode = true
+			btn.button_pressed = false
 			i += 1
 			fact_container.add_child(btn)
 			btn.connect("button_down", fact_btn_pressed.bind(btn, fact, unlocked))
+			all_buttons.append(btn)
