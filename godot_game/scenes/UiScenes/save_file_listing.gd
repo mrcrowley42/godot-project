fb05b6fc886de34eea5d55afce75754f38f9aa3d
mrcrowley42 <mattcrowley42@me.com>
2025-02-21 09:15:35 +1100

    fix icon update

--- godot_game/scenes/UiScenes/save_file_listing.gd
+++ godot_game/scenes/UiScenes/save_file_listing.gd
@@ -16,7 +16,9 @@ func _ready() -> void:
 		date_label.text = lp_date + lp_time
 		var icon_path = Globals.SAVE_ICON_FILE.replace("{}", str(save_file.id))
 		if FileAccess.file_exists(icon_path):
-			creature_icon.icon = load(icon_path)
+			var icon_img = Image.new()
+			icon_img.load(icon_path)
+			creature_icon.icon = ImageTexture.create_from_image(icon_img)
 		#category_icon.tooltip_string = "Category: %s" % sound_node.sound_category.category_name
 		#creature_icon.icon = save_file.icon
 
